54a089e3122ea2173b5e14354d8a84ba8cd164d1
samufromhungary <blzkproblem@gmail.com>
2019-07-15 13:59:54 +0200

    init

---
+++ src/main/java/com/codecool/web/servlet/ScheduleServlet.java
@@ -0,0 +1,40 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.dao.ScheduleDao;
+import com.codecool.web.dao.database.DatabaseScheduleDao;
+import com.codecool.web.model.Account;
+import com.codecool.web.model.Schedule;
+import com.codecool.web.service.ScheduleService;
+import com.codecool.web.service.exception.ServiceException;
+import com.codecool.web.service.simple.SimpleScheduleService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/protected/schedule")
+public final class ScheduleServlet extends AbstractServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            ScheduleDao scheduleDao = new DatabaseScheduleDao(connection);
+            ScheduleService scheduleService = new SimpleScheduleService(scheduleDao);
+
+            String title = req.getParameter("title");
+
+            Schedule schedule = scheduleService.getSchedule(title);
+
+            sendMessage(resp, HttpServletResponse.SC_OK, schedule);
+        } catch (SQLException ex) {
+            handleSqlError(resp, ex);
+        }catch (ServiceException ex) {
+            sendMessage(resp, HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+        }
+    }
+}
